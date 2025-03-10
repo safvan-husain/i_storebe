fd4c14797dc725df9e6eeb0e6d4e2506c2095714
Safvan Husain <developer.safvan@gmail.com>
2025-03-10 16:23:18 +0530

    name added for login response

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -1,116 +1,120 @@
-
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import User from '../../models/User';
-import { generateToken } from '../../utils/jwtUtils';
+import {generateToken} from '../../utils/jwtUtils';
 import asyncHandler from 'express-async-handler';
-import { loginSchema, UserRequestSchema } from './validation';
-import { onCatchError } from '../../middleware/error';
-import { Types } from 'mongoose';
+import {loginSchema, UserRequestSchema} from './validation';
+import {onCatchError} from '../../middleware/error';
+import {Types} from 'mongoose';
 
 export const loginUser = asyncHandler(async (req: Request, res: Response) => {
-  try {
-    const { phone, password } = loginSchema.parse(req.body);
+    try {
+        const {phone, password} = loginSchema.parse(req.body);
 
-    if (!phone || !password) {
-      res.status(400).json({ message: 'Please provide phone and password' });
-      return;
-    }
-    const user = await User.findOne({ phone });
-    if (!user) {
-      res.status(401).json({ message: 'Invalid email or password' });
-      return;
-    }
+        if (!phone || !password) {
+            res.status(400).json({message: 'Please provide phone and password'});
+            return;
+        }
+        const user = await User.findOne({phone});
+        if (!user) {
+            res.status(401).json({message: 'Invalid email or password'});
+            return;
+        }
 
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      res.status(401).json({ message: 'Invalid email or password' });
-      return;
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            res.status(401).json({message: 'Invalid email or password'});
+            return;
+        }
+        let userObject: any = user.toObject();
+        delete userObject.updatedAt;
+        delete userObject.createdAt;
+        delete userObject.password;
+        delete userObject.__v;
+        res.status(200).json({
+            token: generateToken(user),
+            ...userObject
+        });
+    } catch (error) {
+        onCatchError(error, res);
     }
-
-    res.status(200).json({
-      _id: user._id,
-      phone: user.phone,
-      privilege: user.privilege,
-      token: generateToken(user),
-    });
-  } catch (error) {
-    onCatchError(error, res);
-  }
 });
 
 export const createUser = asyncHandler(async (req: Request, res: Response) => {
-  try {
-    let { phone, privilege, manager, ...rest } = UserRequestSchema.parse(req.body);
+    try {
+        let {phone, privilege, manager, ...rest} = UserRequestSchema.parse(req.body);
 
-    // Validate requester permissions
-    const admin = await User.findById(req.userId);
-    if (!['admin', 'manager'].includes(admin?.privilege ?? "")) {
-      res.status(403).json({ message: "You don't have permission to create users" });
-      return;
+        // Validate requester permissions
+        const admin = await User.findById(req.userId);
+        if (!['admin', 'manager'].includes(admin?.privilege ?? "")) {
+            res.status(403).json({message: "You don't have permission to create users"});
+            return;
+        }
+
+        // Handle admin creating manager without specifying a [manager]branch
+        if (admin?.privilege === 'admin' && privilege === 'staff' && !manager) {
+            res.status(400).json({message: "Admin should provide manager(branch) when creating staff"});
+            return;
+        }
+
+        // Enforce manager permissions
+        if (admin?.privilege === 'manager') {
+            if (privilege !== 'staff') {
+                res.status(403).json({message: "You only have permission to create staff"});
+                return;
+            }
+            manager = admin.id;
+        }
+
+        // Check if user already exists
+        const userExists = await User.findOne({phone});
+        if (userExists) {
+            res.status(400).json({message: "User already exists"});
+            return;
+        }
+
+        // Set manager ID for staff creation
+        const managerId = privilege === 'staff' ? manager : undefined;
+
+        // Create user
+        const user = (await User.create({
+            phone,
+            privilege,
+            manager: managerId,
+            ...rest,
+        }));
+
+        let userObject: any = user.toObject();
+        delete userObject.updatedAt;
+        delete userObject.createdAt;
+        delete userObject.password;
+        delete userObject.__v;
+        if (user) {
+            res.status(201).json({
+                token: generateToken(user),
+                ...userObject
+            });
+        } else {
+            res.status(200).json({message: "Failed to create user"});
+        }
+    } catch (error) {
+        onCatchError(error, res);
     }
-
-    // Handle admin creating manager without specifying a [manager]branch
-    if (admin?.privilege === 'admin' && privilege === 'staff' && !manager) {
-      res.status(400).json({ message: "Admin should provide manager(branch) when creating staff" });
-      return;
-    }
-
-    // Enforce manager permissions
-    if (admin?.privilege === 'manager') {
-      if (privilege !== 'staff') {
-        res.status(403).json({ message: "You only have permission to create staff" });
-        return;
-      }
-      manager = admin.id;
-    }
-
-    // Check if user already exists
-    const userExists = await User.findOne({ phone });
-    if (userExists) {
-      res.status(400).json({ message: "User already exists" });
-      return;
-    }
-
-    // Set manager ID for staff creation
-    const managerId = privilege === 'staff' ? manager : undefined;
-
-    // Create user
-    const user = (await User.create({
-      phone,
-      privilege,
-      manager: managerId,
-      ...rest,
-    }));
-
-    let userObject: any = user.toObject();
-    delete userObject.password;
-    if (user) {
-      res.status(201).json({
-        token: generateToken(user),
-        ...userObject
-      });
-    } else {
-      res.status(200).json({ message: "Failed to create user" });
-    }
-  } catch (error) {
-    onCatchError(error, res);
-  }
 });
 
 export const getUsers = asyncHandler(async (req: Request, res: Response) => {
-  const users = await User.find({}).select('-password');
-  res.status(200).json(users);
+    const users = await User.find({}).select('-password');
+    res.status(200).json(users);
 });
 
 export const getUserById = asyncHandler(async (req: Request, res: Response) => {
-  if(!Types.ObjectId.isValid(req.params.id)) {
-    res.status(400).json({ message: 'Invalid user id' })    
-    return;
-  }
-  const user = await User.findById(req.params.id).select('-password');
-  if (user) {
-    res.status(200).json(user);
-  } else {
-    res.status(404).json({ message: 'User not found'});
-  }
+    if (!Types.ObjectId.isValid(req.params.id)) {
+        res.status(400).json({message: 'Invalid user id'})
+        return;
+    }
+    const user = await User.findById(req.params.id).select('-password');
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({message: 'User not found'});
+    }
 });
