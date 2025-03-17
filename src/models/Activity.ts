f0bf063d85e84754f91591944420bb7f40993ce7
Safvan Husain <developer.safvan@gmail.com>
2025-03-17 15:45:13 +0530

    added pre save hood for activity to save the action message if not provided

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -27,6 +27,58 @@ const activitySchema = new Schema<IActivity>(
     }
 );
 
+// Pre-save hook to set the action based on type
+activitySchema.pre('save', async function(next) {
+    try {
+        // Only run this if action is not already set
+        if (!this.action) {
+            // Fetch the user's name from the User collection
+            const User = mongoose.model('User');
+            const user = await User.findById(this.activator);
+            const activatorName = user ? user.name : 'Someone';
+
+            // Set action based on type
+            switch (this.type) {
+                case 'lead_added':
+                    this.action = `${activatorName} created a new lead`;
+                    break;
+                case 'task_added':
+                    this.action = `${activatorName} added a task`;
+                    break;
+                case 'task_updated':
+                    this.action = `${activatorName} updated a task`;
+                    break;
+                case 'lead_updated':
+                    this.action = `${activatorName} updated lead information`;
+                    break;
+                case 'note_added':
+                    this.action = `${activatorName} added a note`;
+                    break;
+                case 'followup_added':
+                    this.action = `${activatorName} added a followup`;
+                    break;
+                case 'status_updated':
+                    this.action = `${activatorName} updated status`;
+                    break;
+                case 'purpose_updated':
+                    this.action = `${activatorName} updated purpose`;
+                    break;
+                case 'check_in':
+                    this.action = `${activatorName} checked in`;
+                    break;
+                case 'check_out':
+                    this.action = `${activatorName} checked out`;
+                    break;
+                default:
+                    this.action = `${activatorName} performed an action`;
+            }
+        }
+        next();
+    } catch (error) {
+        next(error as any);
+    }
+});
+
 const Activity = mongoose.model<IActivity>('Activity', activitySchema);
 
 export default Activity;
