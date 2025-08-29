e0fd0b626d36a633699525daa6b9400bb05baef5
Safvan Husain <developer.safvan@gmail.com>
2025-08-29 11:40:13 +0530

    fix

--- src/controllers/leads/leadController.ts
+++ src/controllers/leads/leadController.ts
@@ -179,10 +179,12 @@ export const updateLeadStatus = asyncHandler(async (req: Request, res: TypedResp
             return;
         }
 
-        if (requestedUser.privilege !== 'admin' && !requestedUser._id.equals(lead.handledBy._id) && updateData.enquireStatus !== lead.enquireStatus) {
+        if (requestedUser.privilege !== 'admin' && !requestedUser._id.equals(lead.handledBy._id)) {
+            if(updateData.enquireStatus && updateData.enquireStatus !== lead.enquireStatus) {
             //the status can only changed by the handler or admin.
             res.status(403).json({ message: "You can't change status since you are not handling this lead" });
             return;
+            } 
         }
 
         let result = await internalLeadStatusUpdate({
